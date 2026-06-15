364802cc5fee424f4b3150b1e74e19e85ab7fb80
LxvAnrel <sergiopferre@icloud.com>
2026-06-15 01:49:19 -0300

    Fix mobile menu always visible before Alpine loads
    Replace x-cloak with inline style="display:none" which works regardless
    of CSS load order. Alpine's x-show overrides it correctly at runtime.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,7 +83,8 @@
             </div>
 
             {{-- Mobile menu --}}
-            <div x-show="open" x-cloak
+            <div x-show="open"
+                 style="display: none;"
                  x-transition:enter="transition ease-out duration-200"
                  x-transition:enter-start="opacity-0 -translate-y-2"
                  x-transition:enter-end="opacity-100 translate-y-0"
